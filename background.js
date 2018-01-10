b445711cb1448bddb9185b1e38331298bb897164
KatyVoor <katyvoor@gmail.com>
2018-01-09 23:25:10 -0500

    Added site as Hacker News

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
 chrome.browserAction.onClicked.addListener(function(activeTab){
-    var newURL = "http://stackoverflow.com/";
+    var newURL = "https://news.ycombinator.com/";
     chrome.tabs.create({ url: newURL });
   });
