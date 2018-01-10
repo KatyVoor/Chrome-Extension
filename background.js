7fee90a3980a99eb53f2a20fcfb917e6e208d516
KatyVoor <katyvoor@gmail.com>
2018-01-09 23:28:02 -0500

    Added some other favorite sites

--- background.js
+++ background.js
@@ -1,4 +1,8 @@
 chrome.browserAction.onClicked.addListener(function(activeTab){
     var newURL = "https://news.ycombinator.com/";
     chrome.tabs.create({ url: newURL });
-  });
+    var newURL = "https://www.npr.org/";
+    chrome.tabs.create({ url: newURL });
+    var newURL = "https://www.npr.org/";
+    chrome.tabs.create({ url: newURL });
+  }); 
